a619355f707c59d171e6e1a748da2da07e0856a1
IsaacMG11 <isaacmg.2006@gmail.com>
2026-09-21 12:47:38 +0200

    Boss Behavior added
    - Attack Queue
    - Boss Health
    - Phase detection

--- Assets/Scripts/Bullets/Shot_Weapons/RadialShotWeapon.cs
+++ Assets/Scripts/Bullets/Shot_Weapons/RadialShotWeapon.cs
@@ -6,42 +6,73 @@ namespace Bullets
     public class RadialShotWeapon : MonoBehaviour
     {
         [SerializeField] private RadialShotPattern shotPattern;
-    
+
         private bool onShotPattern = false;
 
         private void Update()
         {
-            
             if (onShotPattern)
                 return;
-        
+
             StartCoroutine(ExecuteRadialShotPattern(shotPattern));
+        }
+
+        private void OnDisable()
+        {
+            onShotPattern = false;
         }
 
         private IEnumerator ExecuteRadialShotPattern(RadialShotPattern pattern)
         {
             onShotPattern = true;
+
             int lap = 0;
             Vector2 aimDirection = transform.up;
             Vector2 center = transform.position;
 
             yield return new WaitForSeconds(pattern.startWait);
+
             while (lap < pattern.Repetitions)
             {
                 if (lap > 0 && pattern.angleOffsetBetweenReps != 0f)
                     aimDirection = aimDirection.Rotate(pattern.angleOffsetBetweenReps);
-            
+
                 for (int i = 0; i < pattern.patternSettings.Length; i++)
                 {
-                    ShotAttack.RadialShot(center, aimDirection, pattern.patternSettings[i]);
-                    yield return new WaitForSeconds(pattern.patternSettings[i].cooldownTime);
+                    ShotAttack.RadialShot(
+                        center,
+                        aimDirection,
+                        pattern.patternSettings[i]
+                    );
+
+                    yield return new WaitForSeconds(
+                        pattern.patternSettings[i].cooldownTime
+                    );
                 }
+
                 lap++;
             }
-        
+
             yield return new WaitForSeconds(pattern.endWait);
-        
+
             onShotPattern = false;
+        }
+
+        public float GetPatternDuration()
+        {
+            float duration = shotPattern.startWait;
+
+            float lapDuration = 0f;
+
+            foreach (RadialShotSettings settings in shotPattern.patternSettings)
+            {
+                lapDuration += settings.cooldownTime;
+            }
+
+            duration += lapDuration * shotPattern.Repetitions;
+            duration += shotPattern.endWait;
+
+            return duration;
         }
     }
 }
